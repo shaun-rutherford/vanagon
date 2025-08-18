5a75ad91cb5256b715712facb77aa3ebef046018
Shaun Rutherford <srutherford@blizzard.com>
2025-08-18 13:10:26 -0500

    platform must have it's own sudo_bin

--- lib/vanagon/platform.rb
+++ lib/vanagon/platform.rb
@@ -55,7 +55,7 @@ class Vanagon
     attr_accessor :tar
     attr_accessor :shasum
     attr_accessor :brew # This is macOS specific
-    #attr_accessor :sudo_bin
+    attr_accessor :sudo_bin
 
     # Hold a string containing the values that a given platform
     # should use when a Makefile is run - resolves to the CFLAGS
@@ -259,7 +259,7 @@ class Vanagon
       @sort ||= "sort"
       @copy ||= "cp"
       @shasum ||= "sha1sum"
-      #@sudo_bin ||= `sh -c 'command -v sudo'`.strip
+      @sudo_bin ||= `sh -c 'command -v sudo'`.strip
 
       @use_docker_exec = true
       @docker_registry ||= 'docker.io'
@@ -552,7 +552,7 @@ class Vanagon
         "mkdir #{archive_directory}",
         "gunzip -c #{name_and_version}.tar.gz | '#{tar}' -C #{archive_directory} -xf -",
         "rm #{name_and_version}.tar.gz",
-        "cd #{archive_directory}/#{name_and_version}; #{bill_of_materials_command}; #{sudo_bin} chown -R 0:0 #{archive_directory}; #{tar} cf ../../#{name_and_version_and_platform}.tar *",
+        "cd #{archive_directory}/#{name_and_version}; #{bill_of_materials_command}; #{@sudo_bin} chown -R 0:0 #{archive_directory}; #{tar} cf ../../#{name_and_version_and_platform}.tar *",
         "gzip -9c #{name_and_version_and_platform}.tar > #{name_and_version_and_platform}.tar.gz",
         "cp build_metadata.#{name_and_platform}.json output/#{name_and_version_and_platform}.json",
         "cp bill-of-materials output/#{name_and_version_and_platform}-bill-of-materials ||:",
